fc21e0cde5e82bf06dc1e003a59e7e7685b3e30e
Yoon Yeom <yoonyeom@seowoninfo.com>
2024-01-31 16:49:30 +0900

    update

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -3,7 +3,7 @@ import "./TodoTemplate.scss";
 function TodoTemplate({ children }) {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정관리</div>
+      <div className="app-title">일정관리 (react-todo)</div>
       <div className="content">{children}</div>
     </div>
   );
